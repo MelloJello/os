0cca7919c54a8ae225ba76832f587518faa92e49
Michael Wyatt <michaelwyatt@cpe-2607-fcc8-9bcf-ef00-a046-8aad-3d0c-43f7.dyn6.twc.com>
2017-11-23 12:58:46 -0500

    source tree work

---
+++ boot/kernel/init.c
@@ -0,0 +1,8 @@
+//
+//  init.c
+//  
+//
+//  Created by Michael Wyatt on 11/23/17.
+//
+
+#include "init.h"
---
+++ boot/kernel/init.h
@@ -0,0 +1,13 @@
+//
+//  init.h
+//  
+//
+//  Created by Michael Wyatt on 11/23/17.
+//
+
+#ifndef init_h
+#define init_h
+
+#include <stdio.h>
+
+#endif /* init_h */
